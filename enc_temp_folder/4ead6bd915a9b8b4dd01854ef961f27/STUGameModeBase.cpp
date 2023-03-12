31820e5bda46320297b7b17755040e9295744d7d
GholemHub <segeda.olexandr@gmail.com>
2023-03-12 20:03:12 +0100

    HUD class + aim

--- enc_temp_folder/4ead6bd915a9b8b4dd01854ef961f27/STUGameModeBase.cpp
+++ enc_temp_folder/4ead6bd915a9b8b4dd01854ef961f27/STUGameModeBase.cpp
@@ -4,11 +4,9 @@
 #include "STUGameModeBase.h"
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
-#include "UI/STUGameHUD.h"
 
 ASTUGameModeBase::ASTUGameModeBase() 
 {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
-    HUDClass = ASTUGameHUD::StaticClass();
 }

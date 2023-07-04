70442a975a237486c0470e615e2526fa898e749e
GholemHub <segeda.olexandr@gmail.com>
2023-07-04 19:21:09 +0200

    AI move to + BP_AI

---
+++ Source/ShootThemUp/Public/AI/STUAICharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/STUBaseCharacter.h"
+#include "STUAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUAICharacter : public ASTUBaseCharacter
+{
+	GENERATED_BODY()
+	
+};

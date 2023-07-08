dd624b5219731b4eda140ac1e860db38e0743ce5
GholemHub <segeda.olexandr@gmail.com>
2023-07-08 22:07:40 +0200

    EQS Roam + EnemyLocation

---
+++ enc_temp_folder/c165953e856f47c63eacfa3bee5f591b/STUEnemyEnvQueryContext.cpp
@@ -0,0 +1,17 @@
+#include "AI/EQS/STUEnemyEnvQueryContext.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+void USTUEnemyEnvQueryContext::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+    const auto QueryOwner = Cast<AActor>(QueryInstance.Owner.Get());
+
+    const auto Blackboard = UAIBlueprintHelperLibrary::GetBlackboard(QueryOwner);
+    if (!Blackboard)
+        return;
+    const auto ContextActor = Blackboard->GetValueAsObject(EnemyActorName);
+
+    UEnvQueryItemType_Actor::SetContextHelper(ContextData, Cast<AActor>(ContextActor));
+}
